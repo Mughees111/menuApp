9e8f6c5fa749074c9567811aaaa2bcc0c52e9b56
Mughees111 <mughees.abbas@gmail.com>
2021-08-30 10:31:05 +0500

    Created a new Expo app

---
+++ src/components/Svgs.js
@@ -0,0 +1,25 @@
+import * as React from "react"
+import Svg, { Path } from "react-native-svg"
+
+
+
+export function DashboardBlackIcon(props) {
+  return (
+    <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={32}
+      height={32}
+      viewBox="0 0 32 32"
+      {...props}
+    >
+      <Path data-name="Path 3" d="M0 0h32v32H0z" fill="none" />
+      <Path
+        data-name="Path 4"
+        d="M4.333 16.333h8A1.337 1.337 0 0013.667 15V4.333A1.337 1.337 0 0012.333 3h-8A1.337 1.337 0 003 4.333V15a1.337 1.337 0 001.333 1.333zm0 10.667h8a1.337 1.337 0 001.333-1.333v-5.334A1.337 1.337 0 0012.333 19h-8A1.337 1.337 0 003 20.333v5.333A1.337 1.337 0 004.333 27zm13.333 0h8A1.337 1.337 0 0027 25.667V15a1.337 1.337 0 00-1.333-1.333h-8A1.337 1.337 0 0016.333 15v10.667A1.337 1.337 0 0017.667 27zM16.333 4.333v5.334A1.337 1.337 0 0017.667 11h8A1.337 1.337 0 0027 9.667V4.333A1.337 1.337 0 0025.667 3h-8a1.337 1.337 0 00-1.334 1.333z"
+        transform="translate(1 1)"
+        fill="#4a5160"
+      />
+    </Svg>
+  )
+}
+
