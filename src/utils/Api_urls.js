5d29e006f40a8fae9740804748936d8242e38f8d
Jugheadez <78924446+Jugheadez@users.noreply.github.com>
2021-11-14 10:02:24 +0500

    5

--- src/utils/Api_urls.js
+++ src/utils/Api_urls.js
@@ -1,9 +1,20 @@
 export const urls = {
     // API: "http://192.168.100.2/leadme/api/",
     // PHP_API: "http://192.168.100.2/leadme/",
-    API: "http://192.168.100.50/melon/api_vendor/",
-    IMAGEURI : "http://192.168.100.50/melon/resources/uploads/public/",
-    Image_Uri : "http://192.168.100.50/melon/api_vendor/",
+    // API: "https://melon.fictiondevelopers.com/api_vendor/",
+    // IMAGEURI : "http://192.168.100.50/melon/resources/uploads/public/",
+    // Image_Uri : "http://192.168.100.50/melon/api_vendor/",
+
+
+
+
+
+    API: "https://melon.fictiondevelopers.com/api_vendor/",
+    IMAGEURI : "https://melon.fictiondevelopers.com/resources/uploads/public/",
+    Image_Uri : "https://melon.fictiondevelopers.com/api_vendor/",
+
+
+
     // PHP_API: "http://leadme.fubtechx.com/",
     //... more colors here
     MARGIN:false,
